e1a00a99baf7d22e97ccaf1622d91cf30a0a2550
Vaishu1293 <clearexamseasy@gmail.com>
2023-04-07 08:05:10 +0100

    v3

--- fullstackbc/settings.py
+++ fullstackbc/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import ssl
 
@@ -134,3 +134,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
